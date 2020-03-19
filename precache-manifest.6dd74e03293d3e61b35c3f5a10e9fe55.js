9a6f7230a89eeecc82b0c3f6b07338e516009b37
NAQVI <nomanalam7214@gmail.com>
2020-03-19 11:15:51 +0100

    Updates

---
+++ precache-manifest.6dd74e03293d3e61b35c3f5a10e9fe55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6765516a263b6eaa26086bb76355a948",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6aed57478ff01495768d",
+    "url": "./static/js/2.a34c3048.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "./static/js/2.a34c3048.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca18197f83d9a931f01e",
+    "url": "./static/js/main.67eba1d6.chunk.js"
+  },
+  {
+    "revision": "994fcb3a74b4aaef6750",
+    "url": "./static/js/runtime-main.107712e5.js"
+  }
+]);
